57d1b0735f9c26b59ab03cb45ab226d70dc77500
sumitAggarwal416 <55593642+sumitAggarwal416@users.noreply.github.com>
2019-09-20 11:32:32 -0400

    Add files via upload

---
+++ answerThePhone.py
@@ -0,0 +1,18 @@
+#I, Sumit Aggarwal, student number:000793607, certify that I have not copied my
+#code from any other source and that it is my own code. I also certify that I
+#have not allowed anyone else to copy my code.
+user1=input("Are you asleep? ")
+if user1=='yes':
+    print("You don't asnwer the phone")
+else:
+     user2=input("Is your mom calling? ")
+     if user2=='yes':
+         print("You answer the phone")
+     else:
+         user3=input("Is it morning? ")
+         if user3=='yes':
+             print("You don't  answer the phone")
+         else:
+             print("You answer the phone")
+
+        
